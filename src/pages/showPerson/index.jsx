aaab803102ff7f9a7562a319b30005fa2d25fff0
czhhgghm <757563180@qq.com>
2019-09-16 18:10:08 +0800

    添加一个路由页

---
+++ src/pages/showPerson/index.jsx
@@ -0,0 +1,28 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '用户信息'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>用户信息展示</Text>
+      </View>
+    )
+  }
+}
